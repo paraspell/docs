85bc26038a101ed8d4d0ad9fc7af6ed0bbdc166f
dudo50 <dudo50@users.noreply.github.com>
2025-07-26 18:42:44 +0000

    deploy: fd98b70d2ca54f0e438e40bc62ead18f2eeae112

---
+++ assets/index.md.H4YkiYG2.lean.js
@@ -0,0 +1,1 @@
+import{_ as r,c as a,ae as t,o}from"./chunks/framework.DRPMfuGp.js";const g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function l(i,e,s,p,c,h){return o(),a("div",null,e[0]||(e[0]=[t("",25)]))}const u=r(n,[["render",l]]);export{g as __pageData,u as default};
